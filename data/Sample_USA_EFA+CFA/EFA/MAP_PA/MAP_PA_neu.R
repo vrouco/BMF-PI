0f2ecba0be0ce25790c570512a28c04a1234c231
Victor Rouco <victorrouco@ub.edu>
2018-06-26 11:17:36 +0200

    table3

--- data/Sample_USA_EFA+CFA/EFA/MAP_PA/MAP_PA_neu.R
+++ data/Sample_USA_EFA+CFA/EFA/MAP_PA/MAP_PA_neu.R
@@ -1,14 +1,15 @@
 #### EXPLORATORISCHE FAKTORENANALYSE ####
-
-setwd("F:/IPIP Paper/Sample_USA/EFA/MAP_PA")
+library(here)
+library(readxl)
+setwd(here("data/Sample_USA_EFA+CFA/EFA/MAP_PA"))
+#setwd("F:/IPIP Paper/Sample_USA/EFA/MAP_PA")
 # hier bitte Ihren eigenen Pfad eingeben
 
-library(xlsx)
-agree<-read.xlsx("ARI Daten Agreeableness EFA.xls", as.data.frame=TRUE, sheetIndex=1)
-consc<-read.xlsx("conneu.xlsx", as.data.frame=TRUE, sheetIndex=1)
+agree<-read_excel("ARI Daten Agreeableness EFA.xls")
+consc<-read_excel("conneu.xlsx")
 extra_n<-read.csv("ARI Daten Extraversion EFA.csv", header=TRUE, sep=";")
 neuro_n<-read.csv("ARI Daten Neuroticism EFA.csv", header=TRUE, sep=";")
-open<-read.xlsx("ARI Daten Openness EFA.xls", as.data.frame=TRUE, sheetIndex=1, stringAsFactors=F)
+open<-read_excel("ARI Daten Openness EFA.xls")
 open_n<-read.csv("ARI Daten Openness EFA.csv", header=TRUE, sep=";")
 
 agree_items<-agree[c(2:103)]
@@ -39,10 +40,18 @@ vss(open_items)
 # auf anderen Faktoren sind auf Null gesetzt
 # VSS complexity 2: pro Item sind Ladungen auf zwei Faktoren zugelassen
 # MAP empfiehlt ebenfalls eine Anzahl an Faktoren
-# unterer Teil der Ausgabe: Werte für verschiedene Faktorl�sungen: kleinster MAP und größter VSS-Wert entscheidend
+# unterer Teil der Ausgabe: Werte für verschiedene Faktorl?sungen: kleinster MAP und größter VSS-Wert entscheidend
 
 #### Paralleltestanalyse: Eigenwert, Scree-Test und Parallelanalyse ####
-fa.parallel(agree_items, n.iter=1000, fa="PC")
+library(future)
+plan(multiprocess)
+
+
+fa <- future(fa.parallel.poly(agree_items, n.iter=100, fa="PC"))
+resolved(fa)
+value(fa)
+
+
 fa.parallel(consc_items, n.iter=1000, fa="PC")
 fa.parallel(extra_items, n.iter=1000, fa="PC")
 fa.parallel(neuro_items, n.iter=1000, fa="PC")
