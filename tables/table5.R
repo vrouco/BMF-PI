260420f31f6b45d4a18ce95fd0d0ffbf4ad21665
Víctor Rouco <victorrouco@ub.edu>
2018-07-08 21:35:17 +0200

    forward

--- tables/table5.R
+++ tables/table5.R
@@ -4,9 +4,21 @@ colnames(cors)[1] <- "facets"
 
 cors[,-1] <- as.data.frame(sapply(cors[,-1], function(x) ifelse(x >= 0.2 | x <= -0.2, 
                                                                 paste("\\textbf{", x, "}", sep=""), x)))
+corsabrev <- cors[,c(1,8,9,10,11,2,4,6)]
+
+corsabrev[,-1] <- as.data.frame(sapply(corsabrev[,-1], function(x) ifelse(x >= 0.2 | x <= -0.2, 
+                                                                paste("\\textbf{", x, "}", sep=""), x)))
+
 cors <- as.data.frame(lapply(cors, as.character))
+corsabrev <- as.data.frame(lapply(corsabrev, as.character))
+
 
 print(xtable(cors, caption="Criterion correlations"),
+      type="latex",include.rownames=F, size="\\fontsize{5.5pt}{4.5pt}\\selectfont",
+      caption.placement = "top", rotate.colnames = TRUE,
+      sanitize.text.function=Hmisc::latexTranslate, floating.environment = "sidewaystable")
+
+print(xtable(corsabrev, caption="Criterion correlations"),
       type="latex",include.rownames=F, size="\\fontsize{5.5pt}{4.5pt}\\selectfont",
       caption.placement = "top", rotate.colnames = TRUE,
       sanitize.text.function=Hmisc::latexTranslate, floating.environment = "sidewaystable")
