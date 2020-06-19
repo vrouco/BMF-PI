9436dfe64e4b6a178e439955699cc6e5217a7050
Victor Rouco <victorrouco@gmail.com>
2020-06-19 16:13:49 +0200

    more

--- data for ESEM based on partial inv.R
+++ data for ESEM based on partial inv.R
@@ -21,28 +21,30 @@ for(i in 1:length(partial.mi$facet)){
       group.partial=c(paste(compare.int(1), "~1"), 
                       paste(compare.int(2), "~1"))),type = "lv")))
     
-  }else if(!is.na(partial.mi$metric[i]) && partial.mi$metric[i]==T)
+  }else if(!is.na(partial.mi$metric[i]) && partial.mi$metric[i]==T){
   
+    factor_scores[[as.character(partial.mi$facet.short[i])]]<-as.numeric(
+      unlist(lavPredict(cfa(data=data, model = paste(partial.mi$facet.short[i], "=~", paste(key[key$facet==unique(key$facet)[i], "items"],
+                                                                                            collapse=" + ")),group="country", group.equal="loadings"),type = "lv")))
+  }else if(!is.na(partial.mi$partial.metric[i]) && partial.mi$partial.metric[i]==T){
+    
+    factor_scores[[as.character(partial.mi$facet.short[i])]]<-as.numeric(
+      unlist(lavPredict(cfa(data=data, model = paste(partial.mi$facet.short[i], "=~", paste(key[key$facet==unique(key$facet)[i], "items"],
+                                                                                            collapse=" + ")),group="country", group.equal="intercepts",
+                            group.partial=c(paste(partial.mi$facet.short[i], "=~",compare(1)), 
+                                            paste(partial.mi$facet.short[i], "=~",compare(2)))),type = "lv")))
+  }else if(!is.na(partial.mi$partial.metric[i]) && partial.mi$partial.metric[i]==F && partial.mi$metric[i]==F){
+    factor_scores[[as.character(partial.mi$facet.short[i])]]<-as.numeric(
+      unlist(lavPredict(cfa(data=data, model = paste(partial.mi$facet.short[i], "=~", paste(key[key$facet==unique(key$facet)[i], "items"],
+                                                                                            collapse=" + ")),group="country"),type = "lv")))
+  }
+    
+}  
   
-  
-}
-  
-  
-  
-  
-  cfa(data=data, model = paste(partial.mi$facet.short[i], "=~", paste(key[key$facet==unique(key$facet)[i], "items"], collapse=" + ")),
-      group="country", group.equal=c("loadings","intercepts"))
-  
-  factor_scores[,2] <- lavPredict(cfa(data=data, model = paste(partial.mi$facet.short[i], "=~", paste(key[key$facet==unique(key$facet)[i], "items"], collapse=" + ")),
-      group="country", group.equal=c("loadings","intercepts")),type = "lv")
-  
-  
-  as.numeric(unlist(lavPredict(cfa(data=data, model = paste(partial.mi$facet.short[i], "=~", paste(key[key$facet==unique(key$facet)[i], "items"],
-                                                                                                   collapse=" + ")),
-                                   group="country", group.equal=c("loadings","intercepts")),type = "lv")))
-  
-  
-  factor_scores[[as.character(partial.mi$facet.short[i])]]<-as.numeric(unlist(lavPredict(cfa(data=data, model = paste(partial.mi$facet.short[i], "=~", paste(key[key$facet==unique(key$facet)[i], "items"],
-                                                                                                                                           collapse=" + ")),
-                                                                           group="country", group.equal=c("loadings","intercepts")),type = "lv")))
-  
+write.csv(x = factor_scores, file=here::here("factor scores after invariance.csv"))
+write.table(x = factor_scores, file=here::here("factor scores after invariance.txt"), na = "-999", row.names = F, col.names = F)
+
+write.table(x = factor_scores[factor_scores$country=="USA",], file=here::here("factor scores USA after invariance.txt"), na = "-999", row.names = F, col.names = F)
+write.table(x = factor_scores[factor_scores$country=="DE",], file=here::here("factor scores Germany after invariance.txt"), na = "-999", row.names = F, col.names = F)
+
+
