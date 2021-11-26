cf483214774ada44c6eee4a7d1c314207cacc9b0
Victor Rouco <victorrouco@gmail.com>
2021-11-26 12:54:08 +0100

    to share

--- solve error reverse coding germany in cfa.R
+++
@@ -1,7 +0,0 @@
-#solve revrse coding in c5 germany
-
-library(here)
-
-read.table(here("data/Sample_Deutsch_CFA/CFA/Gewissenhaftigkeit/IQIPIP_ohne_Leervaribalen_ohne notes.txt"))
-
-#look at CFA 5 input Mplus code. Variables start in row 7. C5 items are in rows 176, 181, 186, 191, 196
